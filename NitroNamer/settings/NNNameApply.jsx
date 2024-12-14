a14049959452e35b13e5fa04e0c1b24a06bd0d1e
Nitrofix <Nitrofix.yt@gmail.com>
2024-12-14 16:59:11 +0300

    NNNameApply - запись настроек

--- NitroNamer/settings/NNNameApply.jsx
+++ NitroNamer/settings/NNNameApply.jsx
@@ -1,72 +1,181 @@
-function buildApplyUI(thisObj) {
-    var win = thisObj instanceof Panel ? thisObj : new Window("palette", "NNNameApply", undefined, {resizeable: true});
-    win.orientation = "column";
-    win.alignChildren = ["fill", "top"];
-    win.spacing = 10;
-    win.margins = 10;
+// ===================
+// ====== NNNameApply.jsx >>>
 
-    // Группа для кнопок
-    var btnGroup = win.add("group");
-    btnGroup.orientation = "row";
-    btnGroup.alignChildren = ["fill", "center"];
-    btnGroup.spacing = 10;
+function buildApplyUI(a) {
+    var e = a instanceof Panel ? a : new Window("palette", "NNNameApply", undefined, { resizeable: true });
+    e.orientation = "column";
+    e.alignChildren = ["fill", "top"];
+    e.spacing = 10;
+    e.margins = 10;
 
-    // Кнопка "Select All"
-    var btnSelectAll = btnGroup.add("button", undefined, "Select All");
-    btnSelectAll.size = [100, 25];
+    var l = e.add("group");
+    l.orientation = "row";
+    l.alignChildren = ["fill", "center"];
+    l.spacing = 10;
 
-    // Кнопка "Deselect All"
-    var btnDeselectAll = btnGroup.add("button", undefined, "Deselect All");
-    btnDeselectAll.size = [100, 25];
+    var n = l.add("button", undefined, "Select All");
+    n.size = [100, 25];
+    var i = l.add("button", undefined, "Deselect All");
+    i.size = [100, 25];
 
-    // Группа для чекбоксов
-    var chkGroup = win.add("group");
-    chkGroup.orientation = "column";
-    chkGroup.alignChildren = ["left", "top"];
-    chkGroup.spacing = 5;
+    var o = e.add("group");
+    o.orientation = "column";
+    o.alignChildren = ["left", "top"];
+    o.spacing = 5;
 
-    // Список типов слоев
-    var layerTypes = [
-        "Shape Layer",
-        "Text Layer",
-        "Null Object",
-        "Adjustment Layer",
-        "Footage Layer",
-        "Solid Layer",
-        "Pre-Comp",
-        "Camera Layer",
-        "Light Layer"
-    ];
+    var layerTypes = ["Shape Layer", "Text Layer", "Null Object", "Adjustment Layer", "Footage Layer", "Solid Layer", "Pre-Comp", "Camera Layer", "Light Layer"];
+    var checkboxKeys = ["shapeLayer", "textLayer", "nullObject", "adjustmentLayer", "footageLayer", "solidLayer", "preComp", "cameraLayer", "lightLayer"];
+    var t = [];
 
-    var checkboxes = [];
+    // Определение пути к settings.json
+    var scriptFile = new File($.fileName);
+    var scriptFolderPath = scriptFile.path;
+    var settingsFilePath = scriptFolderPath + "/settings.json";
+    var settingsFile = new File(settingsFilePath);
 
-    // Создание чекбоксов
-    for (var i = 0; i < layerTypes.length; i++) {
-        var cb = chkGroup.add("checkbox", undefined, layerTypes[i]);
-        checkboxes.push(cb);
+    // Функции для чтения и записи JSON
+    function readJSONFile(filePath) {
+        var file = new File(filePath);
+        var data = {};
+        if (file.exists) {
+            file.open("r");
+            try {
+                data = eval("(" + file.read() + ")");
+            } catch (e) {
+                alert("Ошибка при разборе JSON-файла: " + filePath, "Ошибка");
+            }
+            file.close();
+        }
+        return data;
     }
 
-    // Функции для кнопок
-    btnSelectAll.onClick = function() {
-        for (var i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].value = true;
+    function writeJSONFile(filePath, data) {
+        var file = new File(filePath);
+        file.encoding = "UTF-8";
+        file.open("w");
+        file.write(JSON.stringify(data, null, 4));
+        file.close();
+    }
+
+    // Функция для инициализации settings.json, если он не существует
+    function initializeSettings() {
+        if (!settingsFile.exists) {
+            var defaultSettings = {
+                "userPresets": {},
+                "currentSettings": {},
+                "selectedLanguage": "Russian",
+                "nameApply": {
+                    "shapeLayer": false,
+                    "textLayer": false,
+                    "nullObject": false,
+                    "adjustmentLayer": false,
+                    "footageLayer": false,
+                    "solidLayer": false,
+                    "preComp": false,
+                    "cameraLayer": false,
+                    "lightLayer": false
+                }
+            };
+            writeJSONFile(settingsFilePath, defaultSettings);
+        } else {
+            // Проверка наличия раздела "nameApply" и его ключей
+            var existingSettings = readJSONFile(settingsFilePath);
+            if (!existingSettings.nameApply) {
+                existingSettings.nameApply = {
+                    "shapeLayer": false,
+                    "textLayer": false,
+                    "nullObject": false,
+                    "adjustmentLayer": false,
+                    "footageLayer": false,
+                    "solidLayer": false,
+                    "preComp": false,
+                    "cameraLayer": false,
+                    "lightLayer": false
+                };
+                writeJSONFile(settingsFilePath, existingSettings);
+            } else {
+                // Убедиться, что все ключи присутствуют
+                var defaultNameApply = {
+                    "shapeLayer": false,
+                    "textLayer": false,
+                    "nullObject": false,
+                    "adjustmentLayer": false,
+                    "footageLayer": false,
+                    "solidLayer": false,
+                    "preComp": false,
+                    "cameraLayer": false,
+                    "lightLayer": false
+                };
+                for (var key in defaultNameApply) {
+                    if (defaultNameApply.hasOwnProperty(key) && existingSettings.nameApply[key] === undefined) {
+                        existingSettings.nameApply[key] = defaultNameApply[key];
+                    }
+                }
+                writeJSONFile(settingsFilePath, existingSettings);
+            }
         }
-    };
+    }
 
-    btnDeselectAll.onClick = function() {
-        for (var i = 0; i < checkboxes.length; i++) {
-            checkboxes[i].value = false;
+    // Инициализация настроек при запуске
+    initializeSettings();
+
+    // Чтение текущих настроек
+    var currentSettings = readJSONFile(settingsFilePath);
+
+    // Создание чекбоксов
+    for (var p = 0; p < layerTypes.length; p++) {
+        var d = o.add("checkbox", undefined, layerTypes[p]);
+        d.value = currentSettings.nameApply[checkboxKeys[p]];
+        t.push(d);
+    }
+
+    // Функция для обновления settings.json при изменении состояния чекбокса
+    function updateSettings(key, value) {
+        var settings = readJSONFile(settingsFilePath);
+        if (!settings.nameApply) {
+            settings.nameApply = {};
         }
-    };
+        settings.nameApply[key] = value;
+        writeJSONFile(settingsFilePath, settings);
+    }
 
-    win.layout.layout(true);
-    win.layout.resize();
+    // Добавление обработчиков событий для чекбоксов
+    for (var p = 0; p < t.length; p++) {
+        (function(index) {
+            t[index].onClick = function() {
+                updateSettings(checkboxKeys[index], this.value);
+            }
+        })(p);
+    }
 
-    return win;
+    // Обработчики кнопок "Select All" и "Deselect All"
+    n.onClick = function() {
+        for (var a = 0; a < t.length; a++) {
+            t[a].value = true;
+            updateSettings(checkboxKeys[a], true);
+        }
+    }
+
+    i.onClick = function() {
+        for (var a = 0; a < t.length; a++) {
+            t[a].value = false;
+            updateSettings(checkboxKeys[a], false);
+        }
+    }
+
+    e.layout.layout(true);
+    e.layout.resize();
+
+    return e;
 }
 
+// Создание и отображение панели
 var applyPanel = buildApplyUI(this);
 if (applyPanel instanceof Window) {
     applyPanel.center();
     applyPanel.show();
+} else {
+    applyPanel.layout.layout(true);
 }
+
+// ===================
